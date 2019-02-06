1765137ff2c67ac3fcd112113d78abe8a0f5e1dc
CFLai1990 <music_lcf@163.com>
2019-02-07 04:03:38 +0800

    Coref used for back end. --ChufanLai

--- apis/annotation/test.py
+++ apis/annotation/test.py
@@ -2,7 +2,7 @@ from flask_socketio import emit
 from .__settings__ import API
 import spacy
 
-nlp = spacy.load('en')
+nlp = spacy.load('en_coref')
 
 class apiClass(API):
   def __init__(self, logger, socket, message, namespace):
