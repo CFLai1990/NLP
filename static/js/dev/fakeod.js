feae675a587c440e7c0a0d73fd56479334ae34d0
CFLai1990 <music_lcf@163.com>
2019-03-04 04:09:53 +0800

    Nothing changed. --ChufanLai

--- static/js/dev/fakeod.js
+++ static/js/dev/fakeod.js
@@ -1,13 +1,40 @@
 const MESSAGE = 'OD_Data'
-const FAKEDATA = {
-  'labels': ['Samsung', '21%', 'Apple', '14%', 'Huawei', '10%', 'Others', '55%']
+const FAKEDATA_PIE = {
+  'axes': [],
+  'labels': ['Samsung', '21%', 'Apple', '14%', 'Huawei', '10%', 'Others', '55%'],
+  'legends': []
+}
+
+const FAKEDATA_BAR = {
+  'axes': [
+    {
+      'label': ['Sales (In Millions)'],
+      'ticks': ['0', '50', '100', '150', '200', '250', '300', '350', '400', '450']
+    }, {
+      'label': ['Year'],
+      'ticks': ['2013', '2014', '2015', '2016', '2017']
+    }
+  ],
+  'labels': [],
+  'legends': [
+    {
+      'label': ['Samsung'],
+      'feature': {'color': 'blue'}
+    }, {
+      'label': ['Apple'],
+      'feature': {'color': 'orange'}
+    }, {
+      'label': ['Huawei'],
+      'feature': {'color': 'green'}
+    }
+  ]
 }
 
 class FakeOD {
   constructor (socket) {
     this.socket = socket
     this.message = MESSAGE
-    this.data = FAKEDATA
+    this.data = FAKEDATA_BAR
     this.handleReceive()
   }
 
