e807d4ebef90cab375a6801bd562833446900b91
CFLai1990 <music_lcf@163.com>
2019-03-04 19:52:19 +0800

    Testing axes and legends. --ChufanLai

--- apis/annotation/libs/pipeline.py
+++ apis/annotation/libs/pipeline.py
@@ -3,6 +3,7 @@ from .label_detection import infer_label
 from .color_detection import infer_color
 from .size_detection import infer_size
 from .location_detection import infer_loc
+from .axis_detection import infer_axis
 
 class NLPPipeline:
     """The pipeline class"""
@@ -21,4 +22,5 @@ class NLPPipeline:
         infer_color(doc, entities)
         infer_size(doc, entities)
         infer_loc(doc, entities)
+        infer_axis(doc, entities, od_data["axes"])
         return entities, labels
