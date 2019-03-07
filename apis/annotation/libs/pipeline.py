85ccc113c6d79704cd1ffc99863be8fea2006f10
CFLai1990 <music_lcf@163.com>
2019-03-08 06:58:56 +0800

    Testing shape detection. --ChufanLai

--- apis/annotation/libs/pipeline.py
+++ apis/annotation/libs/pipeline.py
@@ -1,5 +1,6 @@
 """The pipeline for Natural Language Processing"""
 from .label_detection import infer_label
+from .shape_detection import infer_shape
 from .color_detection import infer_color
 from .size_detection import infer_size
 from .location_detection import infer_loc
@@ -20,6 +21,7 @@ class NLPPipeline:
         #     infer_label(sentence, labels, od_data["labels"])
         # Get the entities described by their visual features
         entities = {}
+        infer_shape(doc, entities)
         infer_label(doc, entities, od_data["labels"])
         infer_legend(doc, entities, od_data["legends"])
         infer_color(doc, entities)
